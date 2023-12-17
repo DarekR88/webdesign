0c700ebe260678c4e7f4cc50e3ff5effe6d50618
Darek Radke <dradmclad@gmail.com>
2023-12-17 17:41:46 -0600

    state and jsx set up for home page image fade feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import MockupImg1 from "../../public/images/deviceMockups.png";
+import MockupImg2 from "../../public/images/deviceMockups2.png";
+import MockupImg3 from "../../public/images/deviceMockups3.png";
 
 export default function Home() {
+  const [isActiveImgOne, setIsActiveImgOne] = useState(true);
+  const [isActiveImgTwo, setIsActiveImgTwo] = useState(true);
+  const [isActiveImgThree, setIsActiveImgThree] = useState(true);
+
+  useEffect(() => {
+    //Implementing the setInterval method
+    const interval = setInterval(() => {
+      if (isActiveImgOne) {
+        setIsActiveImgOne(false);
+        setIsActiveImgTwo(true);
+        setIsActiveImgThree(false);
+      } else if (isActiveImgTwo) {
+        setIsActiveImgOne(false);
+        setIsActiveImgTwo(false);
+        setIsActiveImgThree(true);
+      } else if (isActiveImgThree) {
+        setIsActiveImgOne(true);
+        setIsActiveImgTwo(false);
+        setIsActiveImgThree(false);
+      }
+    }, 4000);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }, [isActiveImgOne, isActiveImgTwo, isActiveImgThree]);
+
   return (
-   <>
-    
-   </>
-  )
+    <>
+      <div className="max-w-[1200px] flex flex-col lg:flex-row m-auto lg:pt-6">
+        <div className="w-screen lg:max-w-600px">
+          <Image src={MockupImg1} alt="mockup1" />
+          <Image src={MockupImg2} alt="mockup2" />
+          <Image src={MockupImg3} alt="mockup3" />
+        </div>
+        <div className="w-screen lg:max-w-600px flex flex-col">
+          <p>Web Design for the modern world</p>
+          <p>Technology and security you can count on for your enterprise.</p>
+        </div>
+      </div>
+    </>
+  );
 }
