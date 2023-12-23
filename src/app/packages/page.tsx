399ae99058b5c503b2d9bb785297f2442cd1b748
Darek Radke <dradmclad@gmail.com>
2023-12-23 14:47:31 -0600

    css packages page

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -6,9 +6,8 @@ import Image from "next/image";
 export default function Packages() {
   return (
     <>
-      <p className="text-center text-6xl py-4 lg:pt-6">Packages</p>
+      <p className="text-6xl py-3 lg:pt-10 lg:pb-8 text-center">Packages</p>
       <div className="flex max-w-[1200px] m-auto">
-        
       </div>
     </>
   );
